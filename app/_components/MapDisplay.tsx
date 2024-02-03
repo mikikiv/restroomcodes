7a48fc974f86d182dcae107010ad68d10ef23ded
Mikey Villavicencio <villavicem@gmail.com>
2024-02-02 22:01:08 -0800

    Get the viewport location working good

--- app/_components/MapDisplay.tsx
+++ app/_components/MapDisplay.tsx
@@ -1,27 +1,54 @@
 "use client"
 
 import { Box } from "@mantine/core"
+import { useLocalStorage } from "@mantine/hooks"
 import mapboxgl from "mapbox-gl"
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useRef } from "react"
 
 export default function MapDisplay() {
 	mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN as string
 
+	
+
 	const mapContainer = useRef<HTMLDivElement | null>(null)
 	const map = useRef<mapboxgl.Map | null>(null)
-	const [lng, setLng] = useState(-70.9)
-	const [lat, setLat] = useState(42.35)
-	const [zoom, setZoom] = useState(9)
+
+	const [viewport, setViewport] = useLocalStorage({
+		key: "mapviewport",
+		defaultValue: {
+			latitude: 42.35,
+			longitude: -70.9,
+			zoom: 9,
+		},
+	})
 
 	useEffect(() => {
 		if (map.current || !mapContainer.current) return
 		map.current = new mapboxgl.Map({
 			container: mapContainer.current,
 			style: "mapbox://styles/mapbox/streets-v12",
-			center: [lng, lat],
-			zoom: zoom,
+			center: [viewport.longitude, viewport.latitude],
+			zoom: viewport.zoom,
+		})
+
+		//add controls
+		map.current.addControl(
+			new mapboxgl.GeolocateControl({
+				positionOptions: { enableHighAccuracy: true },
+				trackUserLocation: true,
+			}),
+		)
+
+		map.current.on("move", () => {
+			setViewport({
+				latitude: map.current?.getCenter().lat as number,
+				longitude: map.current?.getCenter().lng as number,
+				zoom: map.current?.getZoom() as number,
+			})
 		})
 	})
+
+
 
 	return (
 		<Box
