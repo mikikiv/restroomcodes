7c0c79f0ba6c5e8e131ad6396fdf18118edd8dd0
Mikey Villavicencio <villavicem@gmail.com>
2024-02-06 15:19:42 -0800

    Change Database to be next_public

--- app/_lib/supbaseClient.ts
+++ app/_lib/supbaseClient.ts
@@ -1,14 +1,14 @@
 import { createClient } from "@supabase/supabase-js"
 
 if (
-	process.env.DATABASE_URL === undefined ||
+	process.env.NEXT_PUBLIC_DATABASE_URL === undefined ||
 	process.env.SUPABASE_KEY === undefined
 ) {
 	throw new Error(
-		"Please define DATABASE_URL and SUPABASE_KEY in your .env.local file",
+		"Please define NEXT_PUBLIC_DATABASE_URL and SUPABASE_KEY in your .env.local file",
 	)
 }
 export const supabase = createClient(
-	process.env.DATABASE_URL,
+	process.env.NEXT_PUBLIC_DATABASE_URL,
 	process.env.SUPABASE_KEY,
 )
